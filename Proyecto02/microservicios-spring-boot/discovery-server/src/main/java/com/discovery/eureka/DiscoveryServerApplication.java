8d7058d81f807a542809fe7b8b5c649312294a70
David.Pugliese <davplzt@gmail.com>
2023-06-09 19:38:53 -0500

    Implementacion Eureka Server

--- Proyecto02/microservicios-spring-boot/discovery-server/src/main/java/com/discovery/eureka/DiscoveryServerApplication.java
+++ Proyecto02/microservicios-spring-boot/discovery-server/src/main/java/com/discovery/eureka/DiscoveryServerApplication.java
@@ -1,15 +1,16 @@
-package com.msvc.producto;
+package com.discovery.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaClient
-public class ProductoServiceApplication {
+@EnableEurekaServer
+public class DiscoveryServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProductoServiceApplication.class, args);
+		SpringApplication.run(DiscoveryServerApplication.class, args);
 	}
 
 }
